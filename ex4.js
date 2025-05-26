5327d09a279c48a26431a9f05d3e217f437e86fd
Gih <giovannabreinackcolombara@gmail.com>
2025-05-26 14:48:24 -0300

    ex4 finalizado

---
+++ ex4.js
@@ -0,0 +1,11 @@
+const leia = require('readline-sync');
+
+let numero1, numero2, numero3, numero4;
+let diferenca;
+
+numero1 = leia.questionFloat("Digite o primeiro numero: ");
+numero2 = leia.questionFloat("Digite o segundo numero: ");
+numero3 = leia.questionFloat("Digite o terceiro numero: ");
+numero4 = leia.questionFloat("Digite o quarto numero: ");
+diferenca = (numero1 * numero2) - (numero3 * numero4);
+console.log("Diferença: " + diferenca);
